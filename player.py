f1bb7203663e0a325a88d68280a993f96c022a8d
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 17:44:18 -0300

    now it's validating user crescent diagonal win

--- player.py
+++ player.py
@@ -1,8 +1,12 @@
+from random import choices
+
+
 def convert_list_to_string(input_list):
     result = ""
     for element in input_list:
         result += element
     return result
+
 
 class Player():
     def __init__(self, name="User 1") -> None:
@@ -11,14 +15,15 @@ class Player():
         self.status: str = None
 
     def add_choice(self, choice: str):
-        def valid_user_diagonal_win():
-            result = convert_list_to_string(self.choices)
+        def valid_user_crescent_diagonal_win():
+            result = filter(lambda user_choice: user_choice in 'A1B2C3', sorted(self.choices))
+            result = convert_list_to_string(result)
             return result == 'A1B2C3'
 
         if choice.upper() in 'A1B1C1A2C2B2A3B3C3' and len(choice) == 2:
             self.choices.append(choice)
 
-            if len(self.choices) >= 3 and valid_user_diagonal_win():
+            if len(self.choices) >= 3 and valid_user_crescent_diagonal_win():
                 self.status = "winner"
 
         
