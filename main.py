9f4f44dca8a28dd72bcc894f25b2d07361d3319d
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 13:13:43 -0300

    Improving the hash table printed

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-hash_talbe = [['-','-','-'],
-            ['-','-','-'],
+hash_talbe = [['X','O','X'],
+            ['X','-','-'],
             ['-','-','-']]
 
 def print_hash_table():
     def is_the_final_index(index_number: int):
         return index_number == 2
 
-    print('A   B   C')
+    print()
+    print('    A   B   C')
+    print()
     for line_index, line in enumerate(hash_talbe):
+        print(f'{line_index + 1}   ', end="")
         for column_index, column in enumerate(line):
-            print(f'{column}', end=f'{"   " if is_the_final_index(column_index) else " | "}')
-        print('')
+            print(f'{column}', end=f'{"  " if is_the_final_index(column_index) else " | "}')
+        print()
 
         if not(is_the_final_index(line_index)):
-            print('---------')
+            print('   ----------')
+    print()
 
 
 print_hash_table()
