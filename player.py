314617e230023e250163219ab67fa9da5eb96c69
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 16:07:43 -0300

    Create class player and passing the test

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player():
+    def __init__(self, name="User 1") -> None:
+        self.name = name
+        self.choices = []
+
+    def add_choice(self, choice: str):
+        if choice.upper() in 'A1B1C1A2C2B2A3B3C3':
+            self.choices.append(choice)
+        
