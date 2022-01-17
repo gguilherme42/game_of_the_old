150392f7b4589a452556a4f649c64eeeb454f6fe
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 17:17:31 -0300

    add validation to diagonal win. Now test will pass

--- player.py
+++ player.py
@@ -1,3 +1,9 @@
+def convert_list_to_string(input_list):
+    result = ""
+    for element in input_list:
+        result += element
+    return result
+
 class Player():
     def __init__(self, name="User 1") -> None:
         self.name = name
@@ -5,6 +11,14 @@ class Player():
         self.status: str = None
 
     def add_choice(self, choice: str):
+        def valid_user_diagonal_win():
+            result = convert_list_to_string(self.choices)
+            return result == 'A1B2C3'
+
         if choice.upper() in 'A1B1C1A2C2B2A3B3C3' and len(choice) == 2:
             self.choices.append(choice)
+
+            if len(self.choices) >= 3 and valid_user_diagonal_win():
+                self.status = "winner"
+
         
