6ea32f695125d5d8161f66f2e8831ac8a660c309
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 13:18:00 -0300

    Refactoring code of the print_hash_table function

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ hash_talbe = [['X','O','X'],
             ['X','-','-'],
             ['-','-','-']]
 
-def print_hash_table():
-    def is_the_final_index(index_number: int):
-        return index_number == 2
-
+def print_hash_table_columns():
     print()
     print('    A   B   C')
     print()
+
+
+def print_hash_table_line(line_number: int):
+    print(f'{line_number}   ', end="")
+
+
+def print_hash_table():
+    def is_the_final_index(index_number: int):
+        return index_number == 2
+    
+    print_hash_table_columns()
+
     for line_index, line in enumerate(hash_talbe):
-        print(f'{line_index + 1}   ', end="")
+        print_hash_table_line(line_index + 1)
         for column_index, column in enumerate(line):
             print(f'{column}', end=f'{"  " if is_the_final_index(column_index) else " | "}')
         print()
