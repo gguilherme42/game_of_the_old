1acadb98d70f0bba6a9b21385d24a0e0c7b46d17
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 17:07:47 -0300

    Add attribute status to user class. The test will throw assertion error

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ class Player():
     def __init__(self, name="User 1") -> None:
         self.name = name
         self.choices = []
+        self.status: str = None
 
     def add_choice(self, choice: str):
         if choice.upper() in 'A1B1C1A2C2B2A3B3C3' and len(choice) == 2:
