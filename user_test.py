b4e7b0b669db015b49138e630175bb1729c22853
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 17:45:42 -0300

    Testing decrescent diagonal win. Test will fail.

--- user_test.py
+++ user_test.py
@@ -34,6 +34,16 @@ class UserTest(unittest.TestCase):
         self.assertTrue(user_1.status == "winner")
         
 
+    def test_when_the_user_has_five_choices_and__its_a_decrescent_diagonal_win_than_user_status_should_be_winner(self):
+        user_1 = player.Player()
+        user_1.add_choice('A1')
+        user_1.add_choice('A3')
+        user_1.add_choice('B1')
+        user_1.add_choice('B2')
+        user_1.add_choice('C1')
+
+        self.assertTrue(user_1.status == "winner")
+        
 
 
 if __name__ == "__main__":
