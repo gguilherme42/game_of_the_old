e212e39dae72d61119da5812e33cf1ee66a69ce8
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 22:14:14 -0300

    Removing non sense function. Refactoring tests

--- user_test.py
+++ user_test.py
@@ -7,20 +7,14 @@ class UserTest(unittest.TestCase):
     def test_when_the_user_input_its_out_of_range_then_its_choices_are_empty(self):
         user_1 = player.Player()
         user_1.add_choice('C4')
-        
-        def empty_choices():
-            return user_1.choices
-
-        self.assertFalse(empty_choices())
+    
+        self.assertFalse(user_1.choices)
     
     def test_when_the_user_input_its_not_a_string_with_len_2_then_its_choices_are_empty(self):
         user_1 = player.Player()
         user_1.add_choice('C1A')
         
-        def empty_choices():
-            return user_1.choices
-
-        self.assertFalse(empty_choices())
+        self.assertFalse(user_1.choices)
     
     def test_when_the_user_has_five_choices_and__its_a_crescent_diagonal_win_than_user_status_should_be_winner(self):
         user_1 = player.Player()
