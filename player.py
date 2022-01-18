150af6fde40af206b45e5cb06b5b0d7139334264
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 21:50:05 -0300

    Refactoring functions that valids the diagonal win

--- player.py
+++ player.py
@@ -8,16 +8,18 @@ def convert_list_to_string(input_list):
     return result
 
 
-def valid_user_crescent_diagonal_win():
-    result = filter(lambda user_choice: user_choice in 'A1B2C3', sorted(self.choices))
+def valid_user_crescent_diagonal_win(user_choices):
+    crescent_pattern = 'A1B2C3' 
+    result = filter(lambda user_choice: user_choice in crescent_pattern, sorted(user_choices))
     result = convert_list_to_string(result)
-    return result == 'A1B2C3'
+    return result == crescent_pattern
         
 
-def valid_user_decrescent_diagonal_win():
-    result = filter(lambda user_choice: user_choice in 'A3B2C1', sorted(self.choices))
+def valid_user_decrescent_diagonal_win(user_choices):
+    decrescent_pattern = 'A3B2C1'
+    result = filter(lambda user_choice: user_choice in decrescent_pattern, sorted(user_choices))
     result = convert_list_to_string(result)
-    return result == 'A3B2C1'
+    return result == decrescent_pattern
 
 
 def validate_user_win():
