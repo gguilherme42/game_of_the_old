c274d3cd58e660bd4c336622bd08960f1858933b
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 22:15:56 -0300

    Changing names, from user to player, because it's a player class not user

--- user_test.py
+++ user_test.py
@@ -3,39 +3,39 @@ import unittest
 import player
 
 
-class UserTest(unittest.TestCase):
-    def test_when_the_user_input_its_out_of_range_then_its_choices_are_empty(self):
-        user_1 = player.Player()
-        user_1.add_choice('C4')
+class PlayerTest(unittest.TestCase):
+    def test_when_the_player_input_its_out_of_range_then_its_choices_are_empty(self):
+        player_1 = player.Player()
+        player_1.add_choice('C4')
     
-        self.assertFalse(user_1.choices)
+        self.assertFalse(player_1.choices)
     
-    def test_when_the_user_input_its_not_a_string_with_len_2_then_its_choices_are_empty(self):
-        user_1 = player.Player()
-        user_1.add_choice('C1A')
+    def test_when_the_player_input_its_not_a_string_with_len_2_then_its_choices_are_empty(self):
+        player_1 = player.Player()
+        player_1.add_choice('C1A')
         
-        self.assertFalse(user_1.choices)
+        self.assertFalse(player_1.choices)
     
-    def test_when_the_user_has_five_choices_and__its_a_crescent_diagonal_win_than_user_status_should_be_winner(self):
-        user_1 = player.Player()
-        user_1.add_choice('A1')
-        user_1.add_choice('A2')
-        user_1.add_choice('B1')
-        user_1.add_choice('B2')
-        user_1.add_choice('C3')
+    def test_when_the_player_has_five_choices_and__its_a_crescent_diagonal_win_than_player_status_should_be_winner(self):
+        player_1 = player.Player()
+        player_1.add_choice('A1')
+        player_1.add_choice('A2')
+        player_1.add_choice('B1')
+        player_1.add_choice('B2')
+        player_1.add_choice('C3')
 
-        self.assertEqual(user_1.status, "winner")
+        self.assertEqual(player_1.status, "winner")
         
 
-    def test_when_the_user_has_five_choices_and__its_a_decrescent_diagonal_win_than_user_status_should_be_winner(self):
-        user_1 = player.Player()
-        user_1.add_choice('A1')
-        user_1.add_choice('A3')
-        user_1.add_choice('B1')
-        user_1.add_choice('B2')
-        user_1.add_choice('C1')
+    def test_when_the_player_has_five_choices_and__its_a_decrescent_diagonal_win_than_player_status_should_be_winner(self):
+        player_1 = player.Player()
+        player_1.add_choice('A1')
+        player_1.add_choice('A3')
+        player_1.add_choice('B1')
+        player_1.add_choice('B2')
+        player_1.add_choice('C1')
 
-        self.assertEqual(user_1.status, "winner")
+        self.assertEqual(player_1.status, "winner")
         
 
 
