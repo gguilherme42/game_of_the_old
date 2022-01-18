b89d289b4c2aa93f6f304e9e5df9a535fbee1a7d
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 21:46:59 -0300

    Removed the diagional validation winfunctions of the method context

--- player.py
+++ player.py
@@ -8,6 +8,17 @@ def convert_list_to_string(input_list):
     return result
 
 
+def valid_user_crescent_diagonal_win():
+    result = filter(lambda user_choice: user_choice in 'A1B2C3', sorted(self.choices))
+    result = convert_list_to_string(result)
+    return result == 'A1B2C3'
+        
+
+def valid_user_decrescent_diagonal_win():
+    result = filter(lambda user_choice: user_choice in 'A3B2C1', sorted(self.choices))
+    result = convert_list_to_string(result)
+    return result == 'A3B2C1'
+
 
 def validate_user_win():
     return True
@@ -19,15 +30,6 @@ class Player():
         self.status: str = None
 
     def add_choice(self, choice: str):
-        def valid_user_crescent_diagonal_win():
-            result = filter(lambda user_choice: user_choice in 'A1B2C3', sorted(self.choices))
-            result = convert_list_to_string(result)
-            return result == 'A1B2C3'
-        
-        def valid_user_decrescent_diagonal_win():
-            result = filter(lambda user_choice: user_choice in 'A3B2C1', sorted(self.choices))
-            result = convert_list_to_string(result)
-            return result == 'A3B2C1'
 
         if choice.upper() in 'A1B1C1A2C2B2A3B3C3' and len(choice) == 2:
             self.choices.append(choice)
