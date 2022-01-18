9c7c0b934c79ebbd991b649b1a68dd354fdbc2f6
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-17 23:15:14 -0300

    Removing unused code

--- player.py
+++ player.py
@@ -1,26 +1,9 @@
-from random import choices
-
 
 def convert_list_to_string(input_list):
     result = ""
     for element in input_list:
         result += element
     return result
-
-
-def valid_player_crescent_diagonal_win(player_choices):
-    crescent_pattern = 'A1B2C3' 
-    result = filter(lambda player_choice: player_choice in crescent_pattern, sorted(player_choices))
-    result = convert_list_to_string(result)
-    return result == crescent_pattern
-        
-
-def valid_player_decrescent_diagonal_win(player_choices):
-    decrescent_pattern = 'A3B2C1'
-    result = filter(lambda player_choice: player_choice in decrescent_pattern, sorted(player_choices))
-    result = convert_list_to_string(result)
-    return result == decrescent_pattern
-
 
 def validate_if_the_player_won(player_choices):
     win_patterns = [
@@ -66,11 +49,6 @@ class Player():
         def validate_player_choice():
             return choice.upper() in valid_input_options and len(choice) == 2
         
-        
-        # def validate_player_win():
-        #     return valid_player_crescent_diagonal_win(self.choices) \
-        #         or valid_player_decrescent_diagonal_win(self.choices)
-
         if validate_player_choice():
             self.choices.append(choice)
 
