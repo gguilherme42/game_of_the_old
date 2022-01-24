607b5bd690897a0b9cb1bf5ac2a4044fdd5c91bf
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-23 22:09:09 -0300

    Initial form of the application

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from xmlrpc.client import UNSUPPORTED_ENCODING
 from print_table import print_hash_table
+from player import Player
 
-hash_table = [['X','O','X'],
-            ['X','-','-'],
+hash_table = [['-','-','-'],
+            ['-','-','-'],
             ['-','-','-']]
 
 
 
-
 if __name__ == "__main__":
+    user = Player()
     print_hash_table(hash_table)
+    while True:
+
+        user_input = str(input("Posição: "))
+        user.add_choice(user_input)
+        print_hash_table(hash_table)
+        if user.status == "winner":
+            print(f"Você ganhou, {user.name}!")
+            break
+        break
