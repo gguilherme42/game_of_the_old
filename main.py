f67b36ffd5caf2432be7ab92bddc91bc86388af8
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-24 00:34:15 -0300

    ensure players input

--- main.py
+++ main.py
@@ -14,23 +14,55 @@ def convert_line_number_to_valid_index_line_number(line_number: str):
     line_converter = {'1': 0, '2': 1, '3':2}
     return line_converter[line_number]
 
+def is_position_not_ocupated(position: str):
+    column = convert_column_letter_to_number_column(position[0])
+    line = convert_line_number_to_valid_index_line_number(position[1])
+    
+    return HASH_TABLE[line][column] == "-"
 
 def add_choice_to_hash_talbe(position: str, player_marker: str):
     column = convert_column_letter_to_number_column(position[0])
     line = convert_line_number_to_valid_index_line_number(position[1])
-    
-    position_is_not_ocupated = HASH_TABLE[line][column] == "-"
-    if position_is_not_ocupated:
+     
+    if is_position_not_ocupated(position):
         HASH_TABLE[line][column] = player_marker
 
+def input_hash_position(player_name: str):
+    import sys
+    input_pattern = 'A1B1C1A2C2B2A3B3C3'
+
+    while True:
+        try:
+            result = str(input(f"{player_name} -> Posição: ")).strip().upper()
+            if result in input_pattern:
+
+                if is_position_not_ocupated(result):
+                    return result
+        except IndexError:
+            print("POSIÇÃO INVÁLIDA!")
+        
+        except KeyboardInterrupt:
+            sys.exit()
+        else:
+            print("POSIÇÃO INVÁLIDA!")
+
+
 if __name__ == "__main__":
-    user = Player()
+    user = Player("Guilherme")
+    enemy = Player("Enemy", "O")
     print_hash_table(HASH_TABLE)
     while True:
 
-        user_input = str(input("Posição: "))
+        user_input = input_hash_position(user.name)
         user.add_choice(user_input)
+        add_choice_to_hash_talbe(user_input, user.marker)
         print_hash_table(HASH_TABLE)
+
+        enemy_input = input_hash_position(enemy.name)
+        enemy.add_choice(enemy_input)
+        add_choice_to_hash_talbe(enemy_input, enemy.marker)
+        print_hash_table(HASH_TABLE)
+
         if user.status == "winner":
             print(f"Você ganhou, {user.name}!")
             break
