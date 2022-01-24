4b48dd3260b0b2ecca7e77dd9454404c9bff7a2c
gguilhermme42 <58306122+gguilherme42@users.noreply.github.com>
2022-01-24 00:13:04 -0300

    Transfer code to main file

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from xmlrpc.client import UNSUPPORTED_ENCODING
 from print_table import print_hash_table
 from player import Player
 
@@ -6,7 +5,23 @@ HASH_TABLE = [['-','-','-'],
             ['-','-','-'],
             ['-','-','-']]
 
+def convert_column_letter_to_number_column(column_letter: str):
+    column_converter = {'A': 0, 'B': 1, 'C': 2}
 
+    return column_converter[column_letter]
+
+def convert_line_number_to_valid_index_line_number(line_number: str):
+    line_converter = {'1': 0, '2': 1, '3':2}
+    return line_converter[line_number]
+
+
+def add_choice_to_hash_talbe(position: str, player_marker: str):
+    column = convert_column_letter_to_number_column(position[0])
+    line = convert_line_number_to_valid_index_line_number(position[1])
+    
+    position_is_not_ocupated = HASH_TABLE[line][column] == "-"
+    if position_is_not_ocupated:
+        HASH_TABLE[line][column] = player_marker
 
 if __name__ == "__main__":
     user = Player()
--- test_main.py
+++ test_main.py
@@ -1,29 +1,7 @@
 import unittest
 
 from player import Player
-
-
-hash_table = [['-','-','-'],
-        ['-','-','-'],
-        ['-','-','-']]
-
-def convert_column_letter_to_number_column(column_letter: str):
-    column_converter = {'A': 0, 'B': 1, 'C': 2}
-
-    return column_converter[column_letter]
-
-def convert_line_number_to_valid_index_line_number(line_number: str):
-    line_converter = {'1': 0, '2': 1, '3':2}
-    return line_converter[line_number]
-
-
-def add_choice_to_hash_talbe(position: str, player_marker: str):
-    column = convert_column_letter_to_number_column(position[0])
-    line = convert_line_number_to_valid_index_line_number(position[1])
-    
-    position_is_not_ocupated = hash_table[line][column] == "-"
-    if position_is_not_ocupated:
-        hash_table[line][column] = player_marker
+from main import HASH_TABLE, add_choice_to_hash_talbe
 
 
 class PlayerTest(unittest.TestCase):
@@ -32,7 +10,7 @@ class PlayerTest(unittest.TestCase):
         self.enemy = Player('Enemy', 'O')
     
     def tearDown(self):
-        hash_table = [['-','-','-'],
+        HASH_TABLE = [['-','-','-'],
         ['-','-','-'],
         ['-','-','-']]
 
@@ -49,7 +27,7 @@ class PlayerTest(unittest.TestCase):
             ['-','-','-'], # 2
             ['X','-','-']] # 3
         
-        self.assertEqual(hash_table, expected)
+        self.assertEqual(HASH_TABLE, expected)
 
 
     def test_when_user_and_enemy_add_their_choices(self):
@@ -68,7 +46,7 @@ class PlayerTest(unittest.TestCase):
             ['-','O','-'], # 2
             ['X','-','-']] # 3
         
-        self.assertEqual(hash_table, expected)
+        self.assertEqual(HASH_TABLE, expected)
 
     def test_when_enemy_tries_an_ocupated_position(self):
         user_choice = 'A3'
@@ -86,7 +64,7 @@ class PlayerTest(unittest.TestCase):
             ['-','-','-'], # 2
             ['X','-','-']] # 3
         
-        self.assertEqual(hash_table, expected)
+        self.assertEqual(HASH_TABLE, expected)
 
 
 if __name__ == "__main__":
